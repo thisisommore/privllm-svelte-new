b594737de5de70d8fd124a36ca3a65286f5101ce
thisisommore <ommore501@gmail.com>
2026-06-25 12:05:10 +0530

    chat: update layout to add message input and select action for chats

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -7,23 +7,34 @@
 	};
 </script>
 
-<h1>Chat</h1>
-<button onclick={newChat}>New Chaty</button>
-{globalStore.xxdk!.chats.length} Chats
-{#each globalStore.xxdk!.messages as msg (msg.id)}
-	<div class="message">
-		<div class="header">
-			<span class="role">{msg.status}</span>
-			<span class="timestamp">{new Date(msg.timestamp).toLocaleString()}</span>
-		</div>
-		<div class="body" class:rich={msg.text}>{@html msg.text}</div>
+<button onclick={newChat}>New Chat</button>
+<div class="flex">
+	<div>
+		{#each globalStore.xxdk!.chats as c, i (c.id)}
+			<div
+				class="message bg-pink-400"
+				onclick={() => {
+					globalStore.xxdk?.loadChat(i);
+				}}
+			>
+				<div class="header">
+					<span class="role">{c.title}</span>
+				</div>
+			</div>
+		{/each}
 	</div>
-{/each}
-{#each globalStore.xxdk!.chats as c (c.id)}
-	<div class="message bg-pink-400">
-		<div class="header">
-			<span class="role">{c.title}</span>
-		</div>
+	<div>
+		{#each globalStore.xxdk!.messages as msg (msg.id)}
+			<div class="message">
+				<div class="header">
+					<span class="role">{msg.status}</span>
+					<span class="timestamp">{new Date(msg.timestamp).toLocaleString()}</span>
+				</div>
+				<div class="body" class:rich={msg.text}>{@html msg.text}</div>
+			</div>
+		{/each}
+		<input type="text" placeholder="Message ai" />
+		<button>Send</button>
+		<div>{progress.isHealthy ? 'Connected' : 'Connecting'}</div>
 	</div>
-{/each}
-Status: {progress.status}
+</div>
