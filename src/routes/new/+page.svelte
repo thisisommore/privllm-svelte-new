1925e82a2ebb6959b763c84088b1d05d76395a20
thisisommore <ommore501@gmail.com>
2026-06-25 13:51:15 +0530

    add confirm password input

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -4,32 +4,58 @@
 	import { SERVER_PUB_CREDS } from '$lib/api/contants';
 	import { logger } from '$lib/logger';
 	import { XXDK } from '$lib/xxdk/xxdk.svelte';
+	import { onMount } from 'svelte';
 	import { globalStore } from '../../store.svelte';
-	import UI from './ui.svelte';
+	import UI, { type Props } from './ui.svelte';
 	let password = $state('');
+	let confirmPassword = $state<Props['confirmPassword']>({
+		input: '',
+		showConfirmPassword: true
+	});
+
+	// prevents ui jump due to initial state setup
+	let initiating = $state(true);
+
+	onMount(() => {
+		const initDone = localStorage.getItem('INIT_DONE');
+		if (initDone == 'true') {
+			confirmPassword.showConfirmPassword = false;
+		}
+		initiating = false;
+	});
+
+	let error = $state('');
 </script>
 
 <div>
-	<UI
-		onDoneClick={async () => {
-			const initDone = localStorage.getItem('INIT_DONE');
-			let xxdk: XXDK;
+	{#if !initiating}
+		<UI
+			onDoneClick={async () => {
+				const initDone = localStorage.getItem('INIT_DONE');
+				let xxdk: XXDK;
 
-			if (initDone != 'true') {
-				xxdk = await XXDK.new(password);
-				globalStore.xxdk = xxdk;
-				logger.log('[privllm] initXXDK completed');
-				localStorage.setItem('INIT_DONE', 'true');
-				await xxdk.newChat();
-				await xxdk.send('Hello from SvelteKit!', SERVER_PUB_CREDS);
-			} else {
-				logger.log('[privllm] loadXXDK starting');
-				xxdk = await XXDK.load(password);
-				globalStore.xxdk = xxdk;
-				await xxdk.loadChat(0);
-			}
-			await goto(resolve('/chat'));
-		}}
-		bind:password
-	/>
+				if (initDone != 'true') {
+					if (password != confirmPassword.input) {
+						error = 'Password do not match :(';
+						return;
+					}
+					xxdk = await XXDK.new(password);
+					globalStore.xxdk = xxdk;
+					logger.log('[privllm] initXXDK completed');
+					localStorage.setItem('INIT_DONE', 'true');
+					await xxdk.newChat();
+					await xxdk.send('Hello from SvelteKit!', SERVER_PUB_CREDS);
+				} else {
+					logger.log('[privllm] loadXXDK starting');
+					xxdk = await XXDK.load(password);
+					globalStore.xxdk = xxdk;
+					await xxdk.loadChat(0);
+				}
+				await goto(resolve('/chat'));
+			}}
+			bind:password
+			bind:confirmPassword
+			{error}
+		/>
+	{/if}
 </div>
--- src/routes/new/ui.svelte
+++ src/routes/new/ui.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import { progress } from '$lib/xxdk/index.svelte';
-	type Props = {
+	export type Props = {
 		onDoneClick: () => void;
 		password: string;
+		confirmPassword: {
+			input: string;
+			showConfirmPassword: boolean;
+		};
+		error: string;
 	};
-	let { onDoneClick, password = $bindable() }: Props = $props();
+	let {
+		onDoneClick,
+		password = $bindable(),
+		confirmPassword = $bindable(),
+		error
+	}: Props = $props();
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center">
 	<br />
 	<input type="text" placeholder="password" bind:value={password} />
+	{#if confirmPassword.showConfirmPassword}
+		<input type="text" placeholder="confirm password" bind:value={confirmPassword.input} />
+	{/if}
 	<button onclick={onDoneClick}>Done</button>
 
 	<div class="m-4 border-[0.1px] border-green-500">
@@ -18,4 +31,10 @@
 	<div class="m-4 border-[0.1px] border-green-500">
 		Network is: <span id="status">{progress.isHealthy ? 'Healthy' : 'Unhealthy'}</span>
 	</div>
+
+	{#if error}
+		<div class="m-4 border-[0.1px] bg-red-200">
+			Error: {error}
+		</div>
+	{/if}
 </div>
